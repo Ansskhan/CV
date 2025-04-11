fd4759fcc384c6263b9d8c1d723d4b47f6a7a0f5
Ansskhan <Anas771khan@hotmail.com>
2025-04-11 16:36:44 +0500

    I added dark and light theme functionality through JavaScript and completed my CV Professionaly

---
+++ toggletheme.js
@@ -0,0 +1,33 @@
+let body = document.getElementById('body');
+window.onload = function (){
+    let savedtheme = localStorage.getItem('theme')
+    if(savedtheme === 'dark'){
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        document.getElementById('dark-theme').checked = true;
+    }
+    else if (savedtheme === 'light'){
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        document.getElementById('light-theme').checked = true;
+    }
+}
+document.getElementById('light-theme').addEventListener('click', changeTheme);
+document.getElementById('dark-theme').addEventListener('click', changeTheme);
+function changeTheme() {
+    var lighttheme = document.getElementById('light-theme');
+    var darktheme = document.getElementById('dark-theme');
+    if(lighttheme.checked){
+        body.classList.add('light-theme');
+        body.classList.remove('dark-theme');
+        localStorage.setItem('theme','light');
+    }
+    else if(darktheme.checked){
+        body.classList.add('dark-theme');
+        body.classList.remove('light-theme');
+        localStorage.setItem('theme','dark');
+    }
+
+}
+console.log(body);
+
